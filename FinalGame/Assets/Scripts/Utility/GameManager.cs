6eb2752c27d4aae53b35a8f9507c1e3f56c531af
Sageof6Blacks <jaylonjohnson@oakland.edu>
2023-04-22 18:46:45 -0400

    Main Menu, pause menu, tutorial levels added
    Added main menu, pause menu, and tutorial added.

--- FinalGame/Assets/Scripts/Utility/GameManager.cs
+++ FinalGame/Assets/Scripts/Utility/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -321,5 +322,10 @@ public class GameManager : MonoBehaviour
             instance.highScore = 0;
         }
         UpdateUIElements();
+    }
+
+    internal static void LoadScene(string v)
+    {
+        throw new NotImplementedException();
     }
 }
